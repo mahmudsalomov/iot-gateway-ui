4df6d67f9d8efa2aab1f408620a0931d8191312d
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2023-05-09 18:33:40 +0500

    test

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import {Container} from "react-bootstrap";
+
+const MainPage = () => {
+    return(
+        <div>
+            <Container>
+                <h1>asdadadadad</h1>
+            </Container>
+        </div>
+    )
+}
+
+export default MainPage;
